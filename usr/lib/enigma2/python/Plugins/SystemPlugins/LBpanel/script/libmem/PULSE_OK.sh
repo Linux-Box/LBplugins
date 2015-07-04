bb2a75c9a61a76736f9d9e42956d41f1c13a8d61
villak <viptronick@gmail.com>
2015-07-04 12:59:28 +0200

    mod script libmem and language

--- usr/lib/enigma2/python/Plugins/SystemPlugins/LBpanel/script/libmem/PULSE_OK.sh
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/LBpanel/script/libmem/PULSE_OK.sh
@@ -2,14 +2,15 @@
 # by: ††LUCIFER††
 echo ""
 MEMORYUSADA=`free | awk '/Mem:/ {print int(100*$3/$2) ;}'`
-echo "Memoria usada gigablue: $MEMORYUSADA %"
+DECODIFICADOR=`cat /proc/stb/info/model`
+echo "Memoria usada por $DECODIFICADOR: $MEMORYUSADA %"
 [ $MEMORYUSADA -ge 80 ];
 sync
 sleep 2 
 echo 3 > /proc/sys/vm/drop_caches
 echo ""
 MEMORYUSADA=`free | awk '/Mem:/ {print int(100*$3/$2) ;}'`
-echo "Memoria usada gigablue tras liberar: $MEMORYUSADA %"
+echo "Memoria usada por $DECODIFICADOR tras liberar: $MEMORYUSADA %"
 
 
 exit 0
